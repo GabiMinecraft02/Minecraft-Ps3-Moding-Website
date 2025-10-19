9b4da3a54466f86bd47cf23d411f289e0d02249b
GabrielMinecraft02 <gabrielmersesse09@gmail.com>
2025-10-19 18:22:27 +0200

    adapt app.py for googlea128813747473c36.html

--- app.py
+++ app.py
@@ -3,17 +3,25 @@ import os
 
 app = Flask(__name__)
 
+# Route principale
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# Route pour robots.txt
 @app.route("/robots.txt")
 def robots():
     return send_from_directory('.', 'robots.txt')
 
+# Route pour sitemap.xml
 @app.route("/sitemap.xml")
 def sitemap():
     return send_from_directory('.', 'sitemap.xml')
+
+# Route pour fichier Google verification
+@app.route("/googlea128813747473c36.html")
+def google_verification():
+    return send_from_directory('.', 'googlea128813747473c36.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
